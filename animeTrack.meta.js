144210d9a4064bf1ba95f13771a803679923619d
Shahar Aviram <shahara.88@gmail.com>
2025-11-27 22:53:20 +0200

    v1.4.3 — improvements + polished MAL status + worker sync

--- animeTrack.meta.js
+++ animeTrack.meta.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name         AnimeTrack
 // @namespace    https://github.com/ShaharAviram1/AnimeTrack
-// @version      1.4.2
+// @version      1.4.3
 // @description  Fast anime scrobbler for MAL
-// @updateURL    https://raw.githubusercontent.com/ShaharAviram1/AnimeTrack/main/animeTrack.meta.js
-// @downloadURL  https://raw.githubusercontent.com/ShaharAviram1/AnimeTrack/v1.4.2/AnimeTrack.user.js
+// @downloadURL  https://raw.githubusercontent.com/ShaharAviram1/AnimeTrack/v1.4.3/AnimeTrack.user.js
 // ==/UserScript==
